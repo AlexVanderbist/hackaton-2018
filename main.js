dd926ec4afe110792c806c6f34884e0e6156dd8a
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-12-08 00:27:14 +0100

    commit

--- main.js
+++ main.js
@@ -2,20 +2,16 @@ const piblaster = require('pi-servo-blaster.js');
 
 const angleToPercent = angle => Math.floor((angle/180) * 100);
 
-//test
+piblaster.setServoPwm("P1-11", angleToPercent(180) + "%");
+piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
+
 piblaster.setServoPwm("P1-11", angleToPercent(90) + "%");
-// piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
+piblaster.setServoPwm("P1-12", angleToPercent(180) + "%");
 
-// piblaster.setServoPwm("P1-11", angleToPercent(180) + "%");
-// piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
-
-// piblaster.setServoPwm("P1-11", angleToPercent(0) + "%");
-// piblaster.setServoPwm("P1-12", angleToPercent(180) + "%");
-
-// setTimeout(() => {
-//   piblaster.setServoPwm("P1-11", angleToPercent(90) + "%");
-//   piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
-// }, 900);
+setTimeout(() => {
+  piblaster.setServoPwm("P1-11", angleToPercent(180) + "%");
+  piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
+}, 900);
 
 // var curAngle = 90;
 // var direction = 1;
