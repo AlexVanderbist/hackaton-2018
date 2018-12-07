7cd63467d67a3d0f448fcf7df20d7600024173e4
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-12-07 23:47:35 +0100

    90 deg

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var piblaster = require('pi-servo-blaster.js'); 
+var piblaster = require('pi-servo-blaster.js');
 
 function angleToPercent(angle) {
   return Math.floor((angle/180) * 100);
@@ -8,10 +8,11 @@ var curAngle = 0;
 var direction = 1;
 setInterval(() => {
   piblaster.setServoPwm("P1-11", angleToPercent(curAngle) + "%");
+  piblaster.setServoPwm("P1-12", angleToPercent(curAngle) + "%");
   console.log("Settinge at: ", curAngle, angleToPercent(curAngle));
   curAngle += direction;
   // Change direction when it exceeds the max angle.
-  if (curAngle >= 180) {
+  if (curAngle >= 90) {
     direction = -1;
   } else if (curAngle <= 0) {
     direction = 1;
