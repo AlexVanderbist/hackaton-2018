eed608becdb4fd7edb053b763f08e081c4e89d35
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-12-08 00:47:49 +0100

    commit

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const piblaster = require('pi-servo-blaster.js');
 
 const angleToPercent = angle => Math.floor((angle/180) * 100);
 
+// Shoot
 piblaster.setServoPwm("P1-11", angleToPercent(180) + "%");
 piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
 
@@ -13,17 +14,17 @@ setTimeout(() => {
   piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
 }, 900);
 
-// var curAngle = 90;
-// var direction = 1;
-// setInterval(() => {
-//   piblaster.setServoPwm("P1-11", angleToPercent(curAngle) + "%");
-//   piblaster.setServoPwm("P1-12", angleToPercent(180-curAngle) + "%");
-//   console.log("Settinge at: ", curAngle, angleToPercent(curAngle));
-//   curAngle += direction;
-//   // Change direction when it exceeds the max angle.
-//   if (curAngle >= 90) {
-//     direction = -1;
-//   } else if (curAngle <= 0) {
-//     direction = 1;
-//   }
-// }, 10);
+
+var curAngle = 0;
+var direction = 1;
+setInterval(() => {
+  piblaster.setServoPwm("P1-13", angleToPercent(curAngle) + "%");
+  console.log("Settinge at: ", curAngle, angleToPercent(curAngle));
+  curAngle += direction;
+  // Change direction when it exceeds the max angle.
+  if (curAngle >= 180) {
+    direction = -1;
+  } else if (curAngle <= 0) {
+    direction = 1;
+  }
+}, 20);
