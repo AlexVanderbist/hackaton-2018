fa8c37c87a96d3e32c38f450b571abe524cfaa74
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-12-08 00:17:43 +0100

    yolo

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-var piblaster = require('pi-servo-blaster.js');
+const piblaster = require('pi-servo-blaster.js');
 
-function angleToPercent(angle) {
-  return Math.floor((angle/180) * 100);
-}
+const angleToPercent = angle => Math.floor((angle/180) * 100);
 
-var curAngle = 0;
-var direction = 1;
-setInterval(() => {
-  piblaster.setServoPwm("P1-11", angleToPercent(curAngle) + "%");
-  piblaster.setServoPwm("P1-12", angleToPercent(180-curAngle) + "%");
-  console.log("Settinge at: ", curAngle, angleToPercent(curAngle));
-  curAngle += direction;
-  // Change direction when it exceeds the max angle.
-  if (curAngle >= 90) {
-    direction = -1;
-  } else if (curAngle <= 0) {
-    direction = 1;
-  }
-}, 10);
+piblaster.setServoPwm("P1-11", angleToPercent(90) + "%");
+piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
+
+piblaster.setServoPwm("P1-11", angleToPercent(0) + "%");
+piblaster.setServoPwm("P1-12", angleToPercent(180) + "%");
+
+setTimeout(() => {
+  piblaster.setServoPwm("P1-11", angleToPercent(90) + "%");
+  piblaster.setServoPwm("P1-12", angleToPercent(90) + "%");
+}, 900);
+
+// var curAngle = 90;
+// var direction = 1;
+// setInterval(() => {
+//   piblaster.setServoPwm("P1-11", angleToPercent(curAngle) + "%");
+//   piblaster.setServoPwm("P1-12", angleToPercent(180-curAngle) + "%");
+//   console.log("Settinge at: ", curAngle, angleToPercent(curAngle));
+//   curAngle += direction;
+//   // Change direction when it exceeds the max angle.
+//   if (curAngle >= 90) {
+//     direction = -1;
+//   } else if (curAngle <= 0) {
+//     direction = 1;
+//   }
+// }, 10);
