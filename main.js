6cd3aacabf08065198e0075b74df0dec4d874848
Alex Vanderbist <alex.vanderbist@gmail.com>
2018-12-07 23:54:58 +0100

    neg angle

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var curAngle = 0;
 var direction = 1;
 setInterval(() => {
   piblaster.setServoPwm("P1-11", angleToPercent(curAngle) + "%");
-  piblaster.setServoPwm("P1-12", angleToPercent(curAngle) + "%");
+  piblaster.setServoPwm("P1-12", angleToPercent(-curAngle) + "%");
   console.log("Settinge at: ", curAngle, angleToPercent(curAngle));
   curAngle += direction;
   // Change direction when it exceeds the max angle.
