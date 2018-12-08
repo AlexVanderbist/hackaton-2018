a512825699fd71038649c0532099238b74160258
Adriaan <adriaanmarain300@gmail.com>
2018-12-08 04:12:28 +0100

    wip

--- ws-test/ws.js
+++ ws-test/ws.js
@@ -23,65 +23,54 @@ WSS.broadcast = function broadcast(data) {
     });
 };
 
-var NodeWebcam = require('node-webcam');
+var opts = {
+    width: 1280,
+    height: 720,
+    quality: 100,
+    delay: 0,
+    saveShots: false,
+    output: "jpeg",
+    device: false,
+    verbose: true,
 
-var FSWebcam = NodeWebcam.FSWebcam;
-var Webcam = new FSWebcam({
+    // [location, buffer, base64]
+    // Webcam.CallbackReturnTypes
     callbackReturn: "base64",
-    saveShots: false
-});
+};
 
+var NodeWebcam = require('node-webcam');
+var FSWebcam = NodeWebcam.FSWebcam;
+var Webcam = new FSWebcam(opts);
 
 // Main
-
 init();
 
 function init() {
-
     setupHTTP();
-
     setupWebcam();
-
     console.log( "Visit localhost:9090" );
-
 }
 
 function setupHTTP() {
-
     var server = HTTP.createServer();
-
     server.on( "request", function( request, response ) {
-
         response.write( HTML_CONTENT );
-
         response.end();
-
     });
-
     server.listen( port );
-
 }
 
 function setupWebcam() {
-
     function capture() {
-
         Webcam.capture( "picture", function( err, data ) {
-
             if( err ) {
-
                 throw err;
-
             }
+            console.log(data);
 
             WSS.broadcast( data );
-
             setTimeout( capture, 25 );
-
         });
-
     }
-
     capture();
-
 }
