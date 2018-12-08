140e8d27fa5f05b980c98a53ce8b03db6863d9b6
Adriaan <adriaanmarain300@gmail.com>
2018-12-08 04:45:10 +0100

    build

---
+++ react/build/precache-manifest.9f80d4158044bb116f87d1bad70b3cba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f7d83cdce5c7fffb7cd",
+    "url": "/static/js/main.5f7d83cd.chunk.js"
+  },
+  {
+    "revision": "a01567ed5c6fbf765041",
+    "url": "/static/js/1.a01567ed.chunk.js"
+  },
+  {
+    "revision": "5f7d83cdce5c7fffb7cd",
+    "url": "/static/css/main.108a7e29.chunk.css"
+  },
+  {
+    "revision": "da3274a3c3f5d5dd98601b3b34fe2e23",
+    "url": "/index.html"
+  }
+];
