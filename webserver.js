d41215e3de2f314fdabc87703f150b41a137e907
Adriaan <adriaanmarain300@gmail.com>
2018-12-08 01:23:04 +0100

    cors

--- webserver.js
+++ webserver.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const { left, right, shoot } = require("./kek");
+
+app.use(cors());
 
 app.get('/left', function (req, res) {
 
