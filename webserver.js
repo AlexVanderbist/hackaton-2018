76682e002e778d5d67a8948588835e81c8da826c
Adriaan <adriaanmarain300@gmail.com>
2018-12-08 04:51:48 +0100

    post

--- webserver.js
+++ webserver.js
@@ -9,29 +9,29 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'react/build')));
 
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/react/build/index.html'));
-});
-
-app.get('/left', function (req, res) {
+app.post('/left', function (req, res) {
 
   const response = left();
   console.log(response);
   res.send(response);
 });
 
-app.get('/right', function (req, res) {
+app.post('/right', function (req, res) {
 
   const response = right();
   console.log(response);
   res.send(response);
 });
 
-app.get('/shoot-that-mofo', function (req, res) {
+app.post('/shoot-that-mofo', function (req, res) {
 
   console.log(shoot());
   res.send('OK');
 });
+
+/* app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/react/build/index.html'));
+}); */
 
 app.listen(3000);
 console.log('listening on port 3000');
