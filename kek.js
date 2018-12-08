54da88c8c6bb2a0991f1bd41695c1daca166a1bf
Adriaan <adriaanmarain300@gmail.com>
2018-12-08 01:26:32 +0100

    wip

--- kek.js
+++ kek.js
@@ -26,13 +26,13 @@ const move = amount => {
 
 return (module.exports = {
     left() {
-        move(10);
+        return(move(10));
 
         //return 'OK LEFT';
     },
 
     right() {
-        move(-10);
+        return(move(-10));
 
         //return 'OK RIGHT';
     },
