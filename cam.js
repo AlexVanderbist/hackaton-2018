1c4bfcb907544843e5fb208c3bf495d10462fc82
Adriaan <adriaanmarain300@gmail.com>
2018-12-08 03:31:11 +0100

    cam

--- cam.js
+++ cam.js
@@ -1,1 +1,32 @@
-var v4l2camera = require("v4l2camera");
+var NodeWebcam = require( "node-webcam" );
+
+var FSWebcam = NodeWebcam.FSWebcam;
+
+var opts = {
+    width: 1280,
+    height: 720,
+    quality: 100,
+    delay: 0,
+    saveShots: true,
+    output: "jpeg",
+    device: false,
+    verbose: true,
+
+    // [location, buffer, base64]
+    // Webcam.CallbackReturnTypes
+    callbackReturn: "location",
+};
+
+var Webcam = FSWebcam.create( opts );
+Webcam.capture("test_picture", function( err, data ) {
+    console.log(err, data);
+} );
+
+/* Webcam.list( function( list ) {
+    var anotherCam = NodeWebcam.create( { device: list[ 0 ] } );
+}); */
+
+
+/* NodeWebcam.capture( "test_picture", opts, function( err, data ) {
+    var image = "<img src='" + data + "'>";
+}); */
